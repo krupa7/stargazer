fb5d33e3239e6c804002f36d7a947721d94e7094
hp <hp@10.100.20.97>
2016-11-21 11:17:35 +0530

    added comma in msgs..

--- spring_web/src/com/om/web/controller/login/LoginController.java
+++ spring_web/src/com/om/web/controller/login/LoginController.java
@@ -19,7 +19,7 @@ public class LoginController {
 	@RequestMapping("/login.do")
 	public String auth(@ModelAttribute LoginDTO loginDTO, HttpServletRequest request) {
 		HttpSession session = request.getSession(true);
-		System.out.println("inside auth method controller...");
+		System.out.println("inside auth method controller...,,.");
 		System.out.println("login dto \t" + loginDTO);
 		if ("om".equals(loginDTO.getUserid()) && "pass".equals(loginDTO.getPwd())) {
 			System.out.println("Valid user");
